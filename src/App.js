375b247c0822b42d67ffed9d2f656b5e58be3049
jaennpb <jaenn.bucio@gmail.com>
2020-12-28 19:23:53 -0600

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import Output from "./components/UserOutput.js";
+import Input from "./components/UserInput.js";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Input />
+        <Output />
         <Output />
       </div>
     );
