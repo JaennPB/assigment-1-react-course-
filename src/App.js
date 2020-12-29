2863778a7b7e8d645d23383baf296f12db3701eb
jaennpb <jaenn.bucio@gmail.com>
2020-12-28 19:19:49 -0600

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
+import Output from "./components/UserOutput.js";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <Output />
+      </div>
+    );
   }
 }
 export default App;
---
+++ src/components/UserOutput.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Output = (props) => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nibh est,
+        imperdiet vitae ante in, posuere tincidunt mi. Aliquam ac sem nulla.
+        Phasellus justo ipsum, vulputate nec nibh sed, condimentum elementum
+        mauris. Praesent tempor, nisi id varius pharetra, lacus tortor molestie
+        nibh, eget efficitur est ante nec mauris. Nullam eget eros at quam
+        volutpat auctor. Vivamus blandit nec velit ullamcorper congue. Aenean at
+        sodales lacus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id
+        elit bibendum, sagittis magna egestas, ornare erat. Suspendisse potenti.
+        Integer ut massa nulla. Maecenas consectetur mi quis est venenatis
+        viverra. Sed cursus lacus quis viverra euismod. Cras ut massa euismod,
+        ultricies nisl eu, pharetra odio. Etiam vehicula nibh sit amet blandit
+        pharetra. Fusce condimentum, magna in vestibulum varius, metus lacus
+        ornare eros, vitae pulvinar magna eros sit amet nulla. Maecenas
+        hendrerit sem in ultricies tincidunt. Phasellus lacinia dolor libero, a
+        pharetra nisl condimentum porttitor
+      </p>
+    </div>
+  );
+};
+
+export default Output;
